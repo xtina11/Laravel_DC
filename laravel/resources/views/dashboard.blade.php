256ea57e4558517d0727f4828846b4584c6f241e
Tin A. De la Cruz <mockingjay15@yahoo.com>
2017-01-01 02:36:51 +0800

    commit again

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -1,1 +1,44 @@
-<h3>Dashboard</h3>
+@extends('layouts.master')
+
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What do you have to say?</h3></header>
+            <form action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Your Post"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Create Post</button>
+            </form>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>What other people say...</h3></header>
+            <article class="post">
+                <p>abcdefghijklmnopqrstuvwxyz1234567890</p>
+                <div class="info">
+                    Posted by Max on 12 Feb 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+            <article class="post">
+                <p>abcdefghijklmnopqrstuvwxyz1234567890</p>
+                <div class="info">
+                    Posted by Max on 12 Feb 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Like</a> |
+                    <a href="#">Dislike</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Delete</a>
+                </div>
+            </article>
+        </div>
+    </section>
+@endsection
