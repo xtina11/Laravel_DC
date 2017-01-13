195b97a92a8261c757f502b59d5445717c708448
Tin A. De la Cruz <mockingjay15@yahoo.com>
2017-01-13 15:32:57 +0800

    Brand 2nd Commit

--- laravel/database/migrations/2016_12_30_070501_create_users_table.php
+++ laravel/database/migrations/2016_12_30_070501_create_users_table.php
@@ -18,7 +18,9 @@ class CreateUsersTable extends Migration
             $table->timestamps();
             $table->string('email');
             $table->string('first_name');
+            $table->string('last_name');
             $table->string('password');
+            $table->string('phone');
             $table->rememberToken();
         });
     }
